62990cc4b8a41005715b713b75326da9ddd5a76f
Sergey Armodin <mak@arm1.ru>
2026-02-03 11:06:26 +0700

    Add unit tests for portal response parsing (use Value::new)

--- src/tests.rs
+++ src/tests.rs
@@ -6,29 +6,21 @@ use crate::extract_uri_from_map;
 #[test]
 fn test_uri_direct() {
     let mut m: HashMap<String, Value> = HashMap::new();
-    m.insert("uri".to_string(), Value::Str("file:///tmp/s1.png".into()));
-    assert_eq!(extract_uri_from_map(&m).as_deref(), Some("file:///tmp/s1.png"));
+    m.insert(uri.to_string(), Value::new(file:///tmp/s1.png));
+    assert_eq!(extract_uri_from_map(&m).as_deref(), Some(file:///tmp/s1.png));
 }
 
 #[test]
-fn test_results_dict_with_pair() {
-    // construct a dict like @a{sv} with a pair where value is the string
+fn test_results_array_with_string() {
     let mut m: HashMap<String, Value> = HashMap::new();
-    // create dict Value using zvariant::Dict via Value::Dict
-    use zbus::zvariant::{Dict, Value as V, Signature};
-    let mut d: Dict = Dict::new(&Signature::from("s"), &Signature::from("v"));
-    d.add("key", Value::Str("file:///tmp/s2.png")) .unwrap();
-    m.insert("results".to_string(), V::Dict(d));
-    assert_eq!(extract_uri_from_map(&m).as_deref(), Some("file:///tmp/s2.png"));
+    // set results to an array containing the file URI
+    m.insert(results.to_string(), Value::new(vec![Value::new(file:///tmp/s2.png)]));
+    assert_eq!(extract_uri_from_map(&m).as_deref(), Some(file:///tmp/s2.png));
 }
 
 #[test]
 fn test_nested_array() {
     let mut m: HashMap<String, Value> = HashMap::new();
-    use zbus::zvariant::{Array, Signature, Value as V};
-    let mut a: Array = Array::new(&Signature::from("s"));
-    a.append(Value::Str("foo")) .unwrap();
-    a.append(Value::Str("file:///tmp/s3.png")) .unwrap();
-    m.insert("results".to_string(), V::Array(a));
-    assert_eq!(extract_uri_from_map(&m).as_deref(), Some("file:///tmp/s3.png"));
+    m.insert(results.to_string(), Value::new(vec![Value::new(foo), Value::new(file:///tmp/s3.png)]));
+    assert_eq!(extract_uri_from_map(&m).as_deref(), Some(file:///tmp/s3.png));
 }
